2bdeff001a592696972b03d295bd813d6ec8e7a1
Ramiro Rivera <ramarivera@gmail.com>
2016-01-22 18:02:41 -0300

    Agregado IbannerItem, implementado por RSSItem y StaticText

---
+++ TpFinalTDP2015.Persistence/Model/IBannerItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpFinalTDP2015.Persistence.Model
+{
+    public interface IBannerItem
+    {
+        string GetText();
+
+        string GetTitle();
+
+    }
+}
