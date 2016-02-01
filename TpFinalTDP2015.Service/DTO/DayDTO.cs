ec9ce93630e35748946a5c23afe8f0fb5f54d246
martinnarrua <martin94.profugo@gmail.com>
2016-02-01 17:26:07 -0300

    agregados day dto y profile

--- TpFinalTDP2015.Service/DTO/DayDTO.cs
+++ TpFinalTDP2015.Service/DTO/DayDTO.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TpFinalTDP2015.Service.Enum;
 
 namespace TpFinalTDP2015.Service.DTO
 {
     class DayDTO: IDTO
     {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public Days Value { get; set; }
+        public string NameOfDay { get; set; }
     }
 }
