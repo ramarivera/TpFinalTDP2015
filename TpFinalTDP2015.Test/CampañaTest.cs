5528b32ace0d08636b1d7b6d8bc0fe5d72eed3ef
martinnarrua <martin94.profugo@gmail.com>
2016-02-03 19:23:03 -0300

    agregados tests de interseccion de intervalos y campañas y banners activos

---
+++ TpFinalTDP2015.Test/CampañaTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TpFinalTDP2015.Model;
+using TpFinalTDP2015.Model.Enum;
+
+namespace TpFinalTDP2015.Test
+{
+    [TestClass]
+    public class CampañaTest
+    {
+        [TestMethod]
+        public void ActiveForDate_IsActive()
+        {
+            Campaign lCampaign = new Campaign() { Name = "Venta de cactus",Description="Publicidad para la venta de cactus para el vivero XX" };
+
+            DateInterval lInterval = new DateInterval() {Name="Miércoles y Jueves de la primera quincena de febrero", ActiveUntil = new DateTime(2016, 2, 15), ActiveFrom = new DateTime(2016, 2, 1) };
+
+            Day lDay1 = new Day() { Id = 4, Value = Days.Miercoles };
+            Day lDay2 = new Day() { Id = 5, Value = Days.Jueves };
+
+            lInterval.AddActiveDay(lDay1);
+            lInterval.AddActiveDay(lDay2);
+
+            TimeSpan time1 = new TimeSpan(18, 0, 0);
+            TimeSpan time2 = new TimeSpan(21, 0, 0);
+
+            TimeInterval lTimeInterval = new TimeInterval() { End = time2, Start = time1 };
+
+            lInterval.AddActiveHours(lTimeInterval);
+
+            lCampaign.AddDateInterval(lInterval);
+
+            DateTime lDate = new DateTime(2016, 2, 3,19,0,0);
+
+            bool lResult = lCampaign.ActiveForDate(lDate);
+
+            Assert.IsTrue(lResult);
+        }
+    }
+}
