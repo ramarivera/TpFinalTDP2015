c1228c3a9621a096b1fcbd5554481fd37f9ab920
Ramarivera <ramarivera@gmail.com>
2016-02-04 00:30:01 -0300

    Lector de configuracion XML v0.1 (faltan excepciones y controles)

---
+++ xml/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace xml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.Language = new System.Windows.Forms.Label();
+            this.cmbTable = new System.Windows.Forms.ComboBox();
+            this.cmbLanguage = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvProperties = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProperties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.31832F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 81.68168F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(612, 343);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.76392F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.23608F));
+            this.tableLayoutPanel2.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox3, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(606, 56);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tableLayoutPanel3);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(374, 50);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 4;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.95238F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 69.04762F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.Language, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.cmbTable, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.cmbLanguage, 3, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(368, 31);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // Language
+            // 
+            this.Language.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Language.AutoSize = true;
+            this.Language.Location = new System.Drawing.Point(202, 9);
+            this.Language.Name = "Language";
+            this.Language.Size = new System.Drawing.Size(55, 13);
+            this.Language.TabIndex = 2;
+            this.Language.Text = "Language";
+            this.Language.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // cmbTable
+            // 
+            this.cmbTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cmbTable.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTable.FormattingEnabled = true;
+            this.cmbTable.Location = new System.Drawing.Point(62, 3);
+            this.cmbTable.Name = "cmbTable";
+            this.cmbTable.Size = new System.Drawing.Size(127, 21);
+            this.cmbTable.TabIndex = 3;
+            // 
+            // cmbLanguage
+            // 
+            this.cmbLanguage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cmbLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLanguage.FormattingEnabled = true;
+            this.cmbLanguage.Location = new System.Drawing.Point(270, 3);
+            this.cmbLanguage.Name = "cmbLanguage";
+            this.cmbLanguage.Size = new System.Drawing.Size(95, 21);
+            this.cmbLanguage.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Table";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tableLayoutPanel4);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(383, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(220, 50);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 2;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(214, 31);
+            this.tableLayoutPanel4.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvProperties);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 65);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(606, 275);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // dgvProperties
+            // 
+            this.dgvProperties.AllowUserToAddRows = false;
+            this.dgvProperties.AllowUserToDeleteRows = false;
+            this.dgvProperties.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProperties.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvProperties.Location = new System.Drawing.Point(3, 16);
+            this.dgvProperties.Name = "dgvProperties";
+            this.dgvProperties.Size = new System.Drawing.Size(600, 256);
+            this.dgvProperties.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 343);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProperties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Language;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvProperties;
+        private System.Windows.Forms.ComboBox cmbTable;
+        private System.Windows.Forms.ComboBox cmbLanguage;
+    }
+}
+
---
+++ xml/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace xml
+{
+    public partial class Form1 : Form
+    {
+        DataSet dataSet = new DataSet();
+        string path = ".\\Datagrid.xml";
+        XDocument iConfigFile;
+        IDictionary<String, String> iTranslations = new Dictionary<String, String>();
+        string bannerDTO = "BannerDTO"; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            iConfigFile = XDocument.Load(path);
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Stream stream = assembly.GetManifestResourceStream("xml.XmlSchema.xsd");
+            XmlSchemaSet lSchemaSet = new XmlSchemaSet();
+            lSchemaSet.Add(XmlSchema.Read(stream, null));
+
+            iConfigFile.Validate(lSchemaSet,null);
+            PopulateComboBox();
+            cmbTable.SelectedIndex = 0;
+            ReloadLanguageComboBox();
+            cmbLanguage.SelectedIndex = 0;
+            ReloadPropertyDataSet();
+            cmbTable.SelectedIndexChanged += cmbTable_SelectedIndexChanged;
+            cmbLanguage.SelectedIndexChanged += cmbLanguage_SelectedIndexChanged;
+
+        }
+
+        private void ReloadLanguageComboBox()
+        {
+            var query = from entity in iConfigFile.Elements().Elements()
+                        where entity.Attribute("name").Value == this.iTranslations[(string)this.cmbTable.SelectedItem]
+                        select entity;
+
+            this.cmbLanguage.Items.Clear();
+
+            foreach (var lang in query.Elements())
+            {
+                this.cmbLanguage.Items.Add(((XElement)lang).Attribute("name").Value);
+            }
+        }
+
+        private void PopulateComboBox()
+        {
+            IEnumerable<XElement> lXEntityElements = iConfigFile.Elements().Elements();
+
+            var query = from entity in lXEntityElements
+                        select (string)entity.Attribute("name").Value;
+
+            this.cmbTable.Items.Clear();
+
+
+            foreach (string name in query)
+            {
+                StringBuilder tempName = new StringBuilder();
+                tempName.Append(char.ToLower(name[0]));
+
+                foreach (char c in name.Skip(1))
+                {
+                    tempName.Append(c);
+                }
+
+                FieldInfo fieldInfo = this.GetType().GetField(tempName.ToString(), BindingFlags.Public |
+                                              BindingFlags.NonPublic |
+                                              BindingFlags.Instance);
+                string temp = (string) fieldInfo.GetValue(this).ToString();
+                this.iTranslations.Add(name, temp);
+                this.cmbTable.Items.Add(temp);
+            }
+
+           
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Create xml reader
+                XmlReader xmlFile = XmlReader.Create(openFileDialog1.FileName, new XmlReaderSettings());
+                
+                //Read xml to dataset
+                dataSet.ReadXml(xmlFile);
+                //Pass empdetails table to datagridview datasource
+                
+                //Close xml reader
+                xmlFile.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Debugger.Break();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           /* string tabla = this.textBox1.Text;
+
+            dgvProperties.DataSource = dataSet.Tables[tabla];
+            */
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbTable_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.ReloadLanguageComboBox();
+            this.ReloadPropertyDataSet();
+        }
+
+        private void ReloadPropertyDataSet()
+        {
+            
+
+            string lEntityName = (string) this.cmbTable.SelectedItem;
+            string lLanguage = (string)this.cmbLanguage.SelectedItem;
+
+            var query = from entity in this.iConfigFile.Elements().Elements()
+                        where (string)entity.Attribute("name").Value == lEntityName
+                                && (from lang in entity.Elements()
+                                    where (string)lang.Attribute("name").Value == lLanguage
+                                    select lang).FirstOrDefault() != null
+                        select (entity.Element("language"));
+
+            IList<XElement> lXml = query.Elements().ToList();
+            XDocument lXProperties = new XDocument(new XElement("root"));
+
+            foreach (var item in lXml)
+            {
+                lXProperties.Element("root").Add(item);
+            }
+
+            dataSet.ReadXml(lXProperties.CreateReader());
+
+            this.dgvProperties.DataSource = dataSet.Tables[0];
+
+
+
+        }
+
+        private void cmbLanguage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.ReloadPropertyDataSet();
+
+        }
+    }
+}
