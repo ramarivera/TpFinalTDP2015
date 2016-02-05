ebb74065b6b6154bd5d1d31d64923d3e2695f0af
martinnarrua <martin94.profugo@gmail.com>
2016-02-04 23:23:44 -0300

    datetimeresolver

---
+++ TpFinalTDP2015.Service/AutoMapper/DateTimeResolver.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpFinalTDP2015.Service.AutoMapper
+{
+    public class DateTimeResolver : ValueResolver<DateTime, DateTime>
+    {
+        protected override DateTime ResolveCore(DateTime source)
+        {
+            return source == new DateTime(0) ? DateTime.Now : source;
+        }
+    }
+}
