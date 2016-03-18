b30ce51548e27de66eb018c9dcb93eb4154b70f2
Ramiro Rivera <ramarivera@gmail.com>
2016-03-18 17:34:51 -0300

    no compile time errors

---
+++ TpFinalTDP2015.Service/UseCaseControllers/IBannerService.cs
@@ -0,0 +1,9 @@
+﻿using MarrSystems.TpFinalTDP2015.BusinessLogic.Services;
+using MarrSystems.TpFinalTDP2015.Model;
+
+namespace MarrSystems.TpFinalTDP2015.BusinessLogic.UseCaseControllers
+{
+    internal interface IBannerService : ICrudService<Banner>
+    {
+    }
+}
