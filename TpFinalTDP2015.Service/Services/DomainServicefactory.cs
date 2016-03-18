17644144cb66f8674e799fe945bac99072e2b4b8
Ramiro Rivera <ramarivera@gmail.com>
2016-03-18 17:48:31 -0300

    IoC resuelve el grafo

---
+++ TpFinalTDP2015.Service/Services/DomainServicefactory.cs
@@ -0,0 +1,22 @@
+﻿using MarrSystems.TpFinalTDP2015.BusinessLogic.UseCaseControllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarrSystems.TpFinalTDP2015.BusinessLogic.Services
+{
+    class DomainServiceFactory : IDomainServiceFactory
+    {
+        IDomainService IDomainServiceFactory.GetService(Type pType)
+        {
+            throw new NotImplementedException();
+        }
+
+        T IDomainServiceFactory.GetService<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
