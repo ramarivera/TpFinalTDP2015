0a9ee924f82984b58d58fb60272648c0c0cf675f
Ramiro Rivera <ramarivera@gmail.com>
2016-03-26 21:18:03 -0300

    Intercepcion y abertura de politicas de inyeccion

---
+++ TpFinalTDP2015.CrossCutting/LoggingCallHandler.cs
@@ -0,0 +1,48 @@
+﻿using Common.Logging;
+using Microsoft.Practices.Unity.InterceptionExtension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarrSystems.TpFinalTDP2015.CrossCutting
+{
+    public class LoggingCallHandler : ICallHandler
+    {
+        public IMethodReturn Invoke(IMethodInvocation input,GetNextHandlerDelegate getNext)
+        {
+            var lLogger = LogManager.GetLogger(input.MethodBase.ReflectedType);
+
+            lLogger.TraceFormat("Entering {0}::{1} with {2} argument(s)",
+                input.Target, input.MethodBase.Name, input.Arguments.Count);
+
+            for (int i = 0; i < input.Arguments.Count; i++)
+            {
+                var argInfo = input.Arguments.GetParameterInfo(i);
+                var arg = input.Arguments[i];
+                lLogger.TraceFormat("\tName: {0}\t Type: {1} \tValue: {2}",
+                    argInfo.Name, argInfo.ParameterType, arg);
+            }
+
+            IMethodReturn methodReturn = getNext().Invoke(input, getNext);
+
+            if (methodReturn.Exception == null)
+            {
+                lLogger.TraceFormat(" Method {0}::{1} returned: [{2}]",
+                               input.Target, input.MethodBase.Name, methodReturn.ReturnValue);
+            }
+            else
+            {
+                lLogger.ErrorFormat("Method {0}::{1} threw exception {2}",
+                    methodReturn.Exception,
+                    input.Target, input.MethodBase.Name, methodReturn.Exception.ToString());
+            }
+
+            return methodReturn;
+        }
+
+        public int Order { get; set; }
+
+    }
+}
--- TpFinalTDP2015.Service/BootStrapper.cs
+++ TpFinalTDP2015.Service/BootStrapper.cs
@@ -13,63 +13,95 @@ using PostSharp.Patterns.Diagnostics;
 using PostSharp.Extensibility;
 using MarrSystems.TpFinalTDP2015.Model;
 using Common.Logging;
+using Microsoft.Practices.Unity.Configuration;
+using Microsoft.Practices.Unity.InterceptionExtension;
+using MarrSystems.TpFinalTDP2015.CrossCutting;
 
 namespace MarrSystems.TpFinalTDP2015.BusinessLogic
 {
-    public static class BootStrapper
+    public static class BootStrap
     {
-        private static readonly ILog cLogger = LogManager.GetLogger(typeof(BootStrapper));
+        private static readonly ILog cLogger = LogManager.GetLogger(typeof(BootStrap));
+        private static IUnityContainer cContainer;
 
 
-        
         public static void Configure()
         {
-            AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE","TpFinalTDP2015.config");
+            AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", "TpFinalTDP2015.config");
             AutoMapperConfiguration.Configure();
-
-        //   BusinessServiceLocator.Register(() => IoCContainerLocator.Container.Resolve<BannerService>());
-        //   BusinessServiceLocator.Register(() => IoCContainerLocator.Container.Resolve<StaticTextService>());
-        //   BusinessServiceLocator.Register(() => IoCContainerLocator.Container.Resolve<CampaignService>());
-        //   BusinessServiceLocator.Register(() => IoCContainerLocator.Container.Resolve<ScheduleService>());
-        //   BusinessServiceLocator.Register(() => IoCContainerLocator.Container.Resolve<RssSourceService>());
+            InitializeContainer();
+            ConfigureContainer();
+            //   BusinessServiceLocator.Register(() => IoCContainerLocator.Container.Resolve<BannerService>());
+            //   BusinessServiceLocator.Register(() => IoCContainerLocator.Container.Resolve<StaticTextService>());
+            //   BusinessServiceLocator.Register(() => IoCContainerLocator.Container.Resolve<CampaignService>());
+            //   BusinessServiceLocator.Register(() => IoCContainerLocator.Container.Resolve<ScheduleService>());
+            //   BusinessServiceLocator.Register(() => IoCContainerLocator.Container.Resolve<RssSourceService>());
 
 
         }
 
+        private static void InitializeContainer()
+        {
+            cContainer = new UnityContainer();
+        }
 
-        
+        private static void ConfigureContainer()
+        {
+            cContainer.
+                AddNewExtension<Interception>().
+                    Configure<Interception>().
+                    AddPolicy("Logging").
+                    AddMatchingRule<AssemblyMatchingRule>(
+                        new InjectionConstructor(
+                            new InjectionParameter(typeof(IPersistenceFactory).Assembly))).
+                    AddMatchingRule<AssemblyMatchingRule>(
+                        new InjectionConstructor(
+                            new InjectionParameter(typeof(BootStrap).Assembly))).
+                    AddCallHandler<LoggingCallHandler>(
+                        new ContainerControlledLifetimeManager(),
+                        new InjectionConstructor(),
+                        new InjectionProperty("Order", 1));
+
+            cContainer.LoadConfiguration("Registrations");
+
+
+            cContainer.RegisterInstance(cContainer.Resolve<IPersistenceFactory>("IPersistenceFactory"));
+        }
+
+
+
         public static IControllerFactory GetControllerFactory()
         {
             //TODO CAMBIAR NOMBREEEEEE
-            //  var aux = IoCContainerLocator.Container.Resolve<IPersistenceFactory>();
-            var uni = IoCContainerLocator.Container;
-
-            uni.RegisterInstance(typeof(IUnityContainer),"IUnityContainer", uni, new ContainerControlledLifetimeManager());
 
 
-            var hola = uni.Registrations;
+            cContainer.RegisterInstance(typeof(IUnityContainer), "IUnityContainer", cContainer, new ContainerControlledLifetimeManager());
+
+
+            var hola = cContainer.Registrations;
             int i = 0;
             cLogger.Debug("Registrations:");
             foreach (var item in hola)
             {
-                cLogger.DebugFormat("\t[{4}] {0}: {1} => {2} ({3}) ", item.Name ?? "Unnamed", item.RegisteredType, item.MappedToType, item.LifetimeManager, i++);
+                cLogger.DebugFormat("\t[{4}] {0}: {1} => {2} ({3}) ",
+                    item.Name ?? "Unnamed", item.RegisteredType, item.MappedToType, item.LifetimeManager, i++);
             }
 
 
 
-            var aux = (IControllerFactory) uni.Resolve(typeof(IControllerFactory));
+            var aux = (IControllerFactory)cContainer.Resolve(typeof(IControllerFactory));
             // var chau = uni.Resolve<IRepository<StaticText>>();
             //var hola = uni.Resolve<StaticTextService>();
 
-            var hel = uni.Resolve<IStaticTextService>();
-           var st = hel.Read(1);
+            var hel = cContainer.Resolve<IStaticTextService>();
+            var st = hel.Read(1);
 
 
             var con = aux.GetController(typeof(ManageBannerHandler));
 
             return aux;
             //return aux;
-        //    throw new NotImplementedException();
+            //    throw new NotImplementedException();
         }
     }
 }
