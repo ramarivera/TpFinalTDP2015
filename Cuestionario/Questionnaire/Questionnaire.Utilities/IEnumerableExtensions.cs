384d473481a33bcc1e0cd9ebe461bb443792def2
rrivera <rrivera@itsynch.com>
2019-12-01 18:39:51 -0300

    Refactored random question list generation

---
+++ Cuestionario/Questionnaire/Questionnaire.Utilities/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Questionnaire.Utilities
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            Random r = new Random();
+            return source.OrderBy(x => r.Next());
+        }
+    }
+}
